10dc559678a41d78f03b15df25913fe73eaa0e6e
Lyas Spiehler <lspiehler@gmail.com>
2021-10-04 11:49:46 +0000

    improve error handling

--- api/printer/port/create.js
+++ api/printer/port/create.js
@@ -18,7 +18,7 @@ module.exports = function(params, callback) {
             let result = {
                 status: resp.statusCode,
                 headers: [],
-                body: JSON.parse(resp.body)
+                body: resp.body
             }
             callback(false, result);
         }
