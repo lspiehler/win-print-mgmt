e475670957e2c7a9b55c76d1bc2a0007104af04f
Lyas Spiehler <lspiehler@gmail.com>
2021-10-14 14:30:09 +0000

    adding queue and print set functionality

--- api/printer/queue/index.js
+++ api/printer/queue/index.js
@@ -2,10 +2,14 @@ const qlist = require('./list');
 const qcreate = require('./create');
 const qdelete = require('./delete');
 const qgetconfigs = require('./getconfigs');
+const qset = require('./set');
+const qsetconfig = require('./setconfig');
 
 module.exports = {
     list: qlist,
     create: qcreate,
     delete: qdelete,
-    getconfigs: qgetconfigs
+    getconfigs: qgetconfigs,
+    set: qset,
+    setconfig: qsetconfig
 }
