15a44df5291646889a793bda5bc6dce0d21d582e
Lyas Spiehler <lspiehler@gmail.com>
2021-10-18 13:23:06 +0000

    add queue flush and print test page

---
+++ api/printer/queue/flush.js
@@ -0,0 +1,26 @@
+const printer = require('../../../lib/printer');
+
+module.exports = function(params, callback) {
+    new printer.queue.flush(params, function(err, resp) {
+        if(err) {
+            let result = {
+                status: 500,
+                headers: [],
+                body: {
+                    result: 'error',
+                    message: err,
+                    data: null
+                }
+            }
+            callback(err, result);
+        } else {
+            //console.log(resp);
+            let result = {
+                status: 200,
+                headers: [],
+                body: JSON.parse(resp.body)
+            }
+            callback(false, result);
+        }
+    });
+}
