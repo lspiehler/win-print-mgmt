3d55f4ef3c0861e228a5246016a00a60838fd594
Lyas Spiehler <lspiehler@gmail.com>
2021-10-18 21:27:43 +0000

    add driver templates

--- api/printer/driver/gettemplate.js
+++ api/printer/driver/gettemplate.js
@@ -19,7 +19,61 @@ var templates = {
             }
         }
     ],
+    "Dell Printer Driver v2": [
+        false,
+        {
+            options: {
+                dmDefaultSource: 1
+            }
+        },
+        {
+            options: {
+                dmDefaultSource: 2
+            }
+        },
+        {
+            options: {
+                dmDefaultSource: 3
+            }
+        }
+    ],
     "Canon Generic PCL6 Driver": [
+        {
+            options: {
+                dmDuplex: 1,
+                dmColor: 1
+            }
+        },
+        {
+            options: {
+                dmDefaultSource: 1,
+                dmDuplex: 1,
+                dmColor: 1
+            }
+        },
+        {
+            options: {
+                dmDefaultSource: 3,
+                dmDuplex: 1,
+                dmColor: 1
+            }
+        },
+        {
+            options: {
+                dmDefaultSource: 2,
+                dmDuplex: 1,
+                dmColor: 1
+            }
+        },
+        {
+            options: {
+                dmDefaultSource: 264,
+                dmDuplex: 1,
+                dmColor: 1
+            }
+        }
+    ],
+    "Canon Generic UFR II Driver": [
         {
             options: {
                 dmDuplex: 1,
@@ -70,6 +124,11 @@ var templates = {
         {
             options: {
                 dmDefaultSource: 261
+            }
+        },
+        {
+            options: {
+                dmDefaultSource: 262
             }
         }
     ],
